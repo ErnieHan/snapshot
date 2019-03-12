49184fa83305263b817b1114ee8845b294f58173
Ernie <ernie.yeh.t.h@emphasis.com.tw>
2019-03-12 11:33:16 +0800

    Updates

---
+++ precache-manifest.0b7049c10b82ba2f3e1d9b57f471a6f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2192e98dd508219ace7b",
+    "url": "/snapshot/static/js/runtime~main.a47e6c69.js"
+  },
+  {
+    "revision": "d7de06588cfc1e7f65f0",
+    "url": "/snapshot/static/js/main.c4680d2b.chunk.js"
+  },
+  {
+    "revision": "a70d502edd3f09c43d7e",
+    "url": "/snapshot/static/js/2.51fb9ee7.chunk.js"
+  },
+  {
+    "revision": "d7de06588cfc1e7f65f0",
+    "url": "/snapshot/static/css/main.d3ac4957.chunk.css"
+  },
+  {
+    "revision": "0ba0cadca8959d0a86655a2d81051fe9",
+    "url": "/snapshot/index.html"
+  }
+];
